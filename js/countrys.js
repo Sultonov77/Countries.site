e2eb6437fe62faee8ec895bdedfca0491c593854
sultonov77 <samtall758@gmail.com>
2024-06-29 22:38:15 +0500

    Add new feature

--- js/countrys.js
+++ js/countrys.js
@@ -1,72 +1,92 @@
 const countrys = [
-    {
-        id: 1,
-        name: "Wallis and Futuna",
-        capital: "Mata-Utu",
-        population: 11750,
-        flag: "https://flagcdn.com/wf.svg"
-    },
-    {
-        id: 2,
-        name: "Iceland",
-        capital: "Reykjavik",
-        population: 366425,
-        flag: "https://flagcdn.com/is.svg"
-    },
-    {
-        id: 3,
-        name: "Luxembourg",
-        capital: "Luxembourg",
-        population: 632275,
-        flag: "https://flagcdn.com/lu.svg"
-    },
-    {
-        id: 4,
-        name: "Mali",
-        capital: "Bamako",
-        population: 20250834,
-        flag: "https://flagcdn.com/ml.svg"
-    },
-    {
-        id: 5,
-        name: "Comoros",
-        capital: "Moroni",
-        population: 869595,
-        flag: "https://flagcdn.com/km.svg"
-    },
-    {
-        id: 6,
-        name: "Australia",
-        capital: "Canberra",
-        population: 25687041,
-        flag: "https://flagcdn.com/au.svg"
-    },
-    {
-        id: 7,
-        name: "Estonia",
-        capital: "Tallinn",
-        population: 1331057,
-        flag: "https://flagcdn.com/ee.svg"
-    },
-    {
-        id: 8,
-        name: "Canada",
-        capital: "Ottawa",
-        population: 38005238,
-        flag: "https://flagcdn.com/ca.svg"
-    },
-    {
-        id: 9,
-        name: "Belarus",
-        capital: "Minsk",
-        population: 9398861,
-        flag: "https://flagcdn.com/by.svg"
-    },
-    {
-        id: 10,
-        name: "Guyana",
-        capital: "Georgetown",
-        population: 786559,
-        flag: "https://flagcdn.com/gy.svg"
-    }
-]
+  {
+    id: 1,
+    name: "Wallis and Futuna",
+    capital: "Mata-Utu",
+    population: 11750,
+    flag: "https://flagcdn.com/wf.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 2,
+    name: "Iceland",
+    capital: "Reykjavik",
+    population: 366425,
+    flag: "https://flagcdn.com/is.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 3,
+    name: "Luxembourg",
+    capital: "Luxembourg",
+    population: 632275,
+    flag: "https://flagcdn.com/lu.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 4,
+    name: "Mali",
+    capital: "Bamako",
+    population: 20250834,
+    flag: "https://flagcdn.com/ml.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 5,
+    name: "Comoros",
+    capital: "Moroni",
+    population: 869595,
+    flag: "https://flagcdn.com/km.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 6,
+    name: "Australia",
+    capital: "Canberra",
+    population: 25687041,
+    flag: "https://flagcdn.com/au.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 7,
+    name: "Estonia",
+    capital: "Tallinn",
+    population: 1331057,
+    flag: "https://flagcdn.com/ee.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 8,
+    name: "Canada",
+    capital: "Ottawa",
+    population: 38005238,
+    flag: "https://flagcdn.com/ca.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 9,
+    name: "Belarus",
+    capital: "Minsk",
+    population: 9398861,
+    flag: "https://flagcdn.com/by.svg",
+    isLike: false,
+    isSave: false,
+  },
+  {
+    id: 10,
+    name: "Guyana",
+    capital: "Georgetown",
+    population: 786559,
+    flag: "https://flagcdn.com/gy.svg",
+    isLike: false,
+    isSave: false,
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let countryList = document.querySelector(".countries-list");
 let selectCountry = document.querySelector(".select-country");
 let searchSection = document.querySelector(".search-section");
+let likeList = document.querySelector(".like-list");
+let saveList = document.querySelector(".save-list");
+let likeCount = document.querySelector(".like-count");
+let saveCount = document.querySelector(".save-count");
 // cards
 function allCountries(arr, list) {
   list.innerHTML = " ";
@@ -11,11 +15,12 @@ function allCountries(arr, list) {
     let elCapital = document.createElement("p");
     let elPopulation = document.createElement("p");
     let elIdtag = document.createElement("span");
+    let elLike = document.createElement("button");
+    let elSave = document.createElement("button");
     let elMore = document.createElement("button");
 
     elItem.className =
       "w-[300px]  hover:scale-105 duration-300 z-0 p-2 m-6 bg-stone-100 rounded-[10px]";
-
     elImg.src = value.flag;
     elImg.className = "w-[100%] h-[200px] rounded-[10px] ";
     elName.textContent = "Country: " + value.name;
@@ -25,15 +30,75 @@ function allCountries(arr, list) {
     elPopulation.textContent = "Population: " + value.population;
     elPopulation.className = "text-[18px] text-zinc-700";
     elIdtag.textContent = "Country: " + value.id;
-    elIdtag.className = "text-[18px] text-teal-800";
-    elMore.textContent = "More info";
+    elIdtag.className = "text-[18px] text-teal-800 flex";
+    elLike.innerHTML = `
+    <svg id = ${value.id} width="30px" height="30px" viewBox="0 0 24 24" fill=${
+      value.isLike ? "red" : "white"
+    } xmlns="http://www.w3.org/2000/svg">
+<path  id = ${
+      value.id
+    } fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+    elLike.className = `border border-slate-700  rounded-md  m-2 hover:bg-red-500 hover:text-white transition-all duration-400`;
+    elLike.id = value.id;
+    elSave.innerHTML = `
+    <svg id = ${
+      value.id
+    } width="30px" height="30px" class="scale-125" viewBox="0 0 24 24"  fill=${
+      value.isSave ? "blue" : "white"
+    } xmlns="http://www.w3.org/2000/svg">
+<path id = ${
+      value.id
+    } fill-rule="evenodd" clip-rule="evenodd" d="M6.75 6L7.5 5.25H16.5L17.25 6V19.3162L12 16.2051L6.75 19.3162V6ZM8.25 6.75V16.6838L12 14.4615L15.75 16.6838V6.75H8.25Z" />
+</svg`;
+    elSave.className = `border border-slate-700 rounded-md m-2  hover:bg-blue-500 hover:text-white transition-all duration-400 `;
+    elSave.id = value.id;
     elMore.className =
-      "text-md ml-[20px] border-slate-700 border rounded-md px-3 py-1 hover:bg-blue-950 hover:text-white transition-all duration-500";
-    elItem.append(elImg, elName, elCapital, elPopulation, elIdtag, elMore);
+      "text-md  flex border-slate-700 border rounded-md px-3 py-1 hover:bg-blue-900 hover:text-white transition-all duration-500";
+
+    elMore.textContent = "More info";
+
+    elItem.append(
+      elImg,
+      elName,
+      elCapital,
+      elPopulation,
+      elIdtag,
+      elLike,
+      elSave,
+      elMore
+    );
     list.append(elItem);
+
+    elLike.addEventListener("click", (e) => {
+      let findObject = countrys.find((item) => item.id == e.target.id);
+      findObject.isLike = !findObject.isLike;
+      allCountries(countrys, countryList);
+      likeCount.textContent = countrys.filter(
+        (item) => item.isLike == true
+      ).length;
+    });
+    elSave.addEventListener("click", (e) => {
+      let findObject1 = countrys.find((item) => item.id == e.target.id);
+      findObject1.isSave = !findObject1.isSave;
+      allCountries(countrys, countryList);
+      saveCount.textContent = countrys.filter(
+        (item) => item.isSave == true
+      ).length;
+    });
   });
 }
 allCountries(countrys, countryList);
+// liked countries list
+likeList.addEventListener("click", () => {
+  const likedList = countrys.filter((item) => item.isLike == true);
+  allCountries(likedList, countryList);
+});
+// saved countries list
+saveList.addEventListener("click", () => {
+  const savedList = countrys.filter((item) => item.isSave == true);
+  allCountries(savedList, countryList);
+});
 // option section
 countrys.forEach((value) => {
   let elOption = document.createElement("option");
